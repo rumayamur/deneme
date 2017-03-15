1a5076ff76ae7703fc83712dd14d47b137dea887
rumayamur <rumayamur@gmail.com>
2017-03-15 23:39:59 +0300

    view pager add

--- Paw/app/src/main/java/com/yagmur/paw/Adapter/ViewPagerAdapter.java
+++ Paw/app/src/main/java/com/yagmur/paw/Adapter/ViewPagerAdapter.java
@@ -2,17 +2,49 @@ package com.yagmur.paw.Adapter;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
-import android.view.LayoutInflater;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.yagmur.paw.R;
 
-/**
- * Created by Yagmur on 28.2.2017. ''
- */
-
 public class ViewPagerAdapter extends PagerAdapter {
+    Context context;
+    private int[] GalImages = new int[]{
+            R.drawable.fifth,
+            R.drawable.third,
+            R.drawable.sixth
+    };
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return GalImages.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((ImageView) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(context);
+        int padding = context.getResources().getDimensionPixelSize(R.dimen.activity_horizontal_margin);
+        imageView.setPadding(padding, padding, padding, padding);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setImageResource(GalImages[position]);
+        ((ViewPager) container).addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ((ViewPager) container).removeView((ImageView) object);
+    }
 }
+
