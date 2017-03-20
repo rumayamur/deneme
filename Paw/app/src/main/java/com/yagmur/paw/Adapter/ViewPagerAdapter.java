8b25cc3e67c96786f8820f27347b11157efd114c
rumayamur <rumayamur@gmail.com>
2017-03-20 22:47:12 +0300

    düzeltme

--- Paw/app/src/main/java/com/yagmur/paw/Adapter/ViewPagerAdapter.java
+++ Paw/app/src/main/java/com/yagmur/paw/Adapter/ViewPagerAdapter.java
@@ -2,7 +2,6 @@ package com.yagmur.paw.Adapter;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
